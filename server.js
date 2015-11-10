5eb7fe9f205e4948c5e29b5be75347eaa6c72f5d
adobley <aidan@obley.net>
2015-11-10 14:31:50 -0500

    Using massive

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
-var config     = require('./config/config.js')(),
-    app        = require('./config/express.js')(),
-    models     = require('./models');
+var config           = require('./config/config.js')(),
+    app              = require('./config/express.js')(),
+    massive          = require('massive'),
+    connectionString = 'postgres://postgres:postgres@localhost:5432/yadaguru'; // TODO: Move to settings file
 
-// Should be refactored, using models.users isn't obvious without looking at models/index.js
-require('./config/passport.js')(models.users);
+// connect to Massive and get DB, loading tables, functions, ect
+var massiveInstance = massive.connectSync({ connectionString : connectionString });
 
-for (var model in models) {
-  if (models.hasOwnProperty(model)) {
-    // Skip sequelize exports
-    if (model.toLowerCase() === 'sequelize' ) {
-      continue;
-    }
-    // Get router for model
-    var router = null;
-    // Get path of model route
-    var path = './routes/' + model + 'Routes.js';
-    try {
-      // Check for a model specific route
-      router = require(path)(models[model]); // Throws error if file not found
-    } catch (e) {
-      // If no route for model, use base route
-      router = require('./routes/baseRoutes.js')(models[model]);
-    }
-    // Convert camelCase to dash-case for url
-    var routeName = model.replace(/\W+/g, '-')
-      .replace(/([a-z\d])([A-Z])/g, '$1-$2')
-      .toLowerCase();
-    // Register route and report to console
-    app.use('/api/' + routeName, router);
-    console.log('Registered route ' + routeName);
-  }
-}
+// Set a reference to the massive instance on app
+app.set('db', massiveInstance);
 
 app.listen(config.port, function () {
   console.log('Running on PORT: ' + config.port);
