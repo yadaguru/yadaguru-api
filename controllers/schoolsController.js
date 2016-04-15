323a96471f0775c9ce9cdca8c0e7d230345f88eb
Mac-lp3 <staticlp3@gmail.com>
2016-04-14 20:08:18 -0400

    @MAC-LP3 completed controller logic for schools controller

--- controllers/schoolsController.js
+++ controllers/schoolsController.js
@@ -11,6 +11,78 @@ var schoolsController = function(schoolsService) {
         res.send(schools);
 	};
 
+	var getById = function (req, res) {
+
+		// TODO get user ID from header token
+		var userId = '';
+
+		var schoolId = req.params.schoolId;
+
+		var school = schoolsService.getByIdAndUserId(schoolId, userId);
+
+		res.status(200);
+        res.send(school);
+	};
+
+	var post = function (req, res) {
+
+		// TODO get user ID from header token
+		var userId = '';
+
+		var tempSchool = {
+			userId : userId,
+			name : req.body.name,
+			dueDate : req.body.dueDate,
+			isActive : req.body.isActive
+		};
+
+		var schools = schoolsService.create(tempSchool);
+
+		res.status(200);
+        res.send(schools);
+	};
+
+	var put = function (req, res) {
+
+		// TODO get user ID from header token
+		var userId = '';
+
+		var schoolId = req.params.schoolId;
+
+		var tempSchool = schoolsService.getByIdAndUserId(schoolId, userId);
+
+		tempSchool.name = req.body.name;
+		tempSchool.dueDate = req.body.dueDate;
+		tempSchool.isActive = req.body.isActive;
+
+		var schools = schoolsService.update(tempSchool);
+
+		res.status(200);
+        res.send(schools);
+	};
+
+	var delete = function (req, res) {
+
+		// TODO get user ID from header token
+		var userId = '';
+
+		var schoolId = req.params.schoolId;
+
+		var isReal = schoolsService.exists(schoolId, userId);
+
+		if (isReal) {
+
+			schoolsService.delete(schoolId);
+			res.status(200);
+
+		} else {
+
+			// the school does not exist, or is not assigned to this user
+			res.status(404);
+		}
+
+	};
+
 };
 
 module.exports = schoolsController;
