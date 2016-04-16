2323451c94f2e360be332c3d59b92b88a7feadb5
Bob D'Errico <bob@bobderrico.com>
2016-04-15 23:05:08 -0400

    [bob] Writes test handling requests with missing optional parameters on baseReminders.

--- test/unit/baseRemindersController_spec.js
+++ test/unit/baseRemindersController_spec.js
@@ -33,12 +33,12 @@ var mockBaseReminderService = {
 
   create: function(data) {
     mockBaseReminders.push({
-      id: 3,
+      id: mockBaseReminders.length + 1,
       name: data.name,
       message: data.message,
       detail: data.detail,
-      lateMessage: data.lateMessage,
-      lateDetail: data.lateDetail,
+      lateMessage: data.lateMessage || '',
+      lateDetail: data.lateDetail || '',
       timeframes: data.timeframes,
       category: data.category
     });
@@ -195,6 +195,70 @@ describe('BaseReminders Controller', function() {
 
   });
 
+  it('should accept a request with optional items when creating a new baseReminder', function() {
+
+    var req = {
+      body: {
+        name: 'A 4th reminder',
+        message: 'A 4th message',
+        detail: 'Yet more details',
+        timeframes: [5],
+        category: 3
+      }
+    };
+
+    var res = {
+      status: sinon.spy(),
+      send: sinon.spy()
+    };
+
+    baseRemindersController.post(req, res);
+    assert.ok(res.status.calledWith(200));
+    assert.ok(res.send.calledWith([
+      {
+        id: 1,
+        name: 'A reminder',
+        message: 'A message',
+        detail: 'Some details',
+        lateMessage: 'A late message',
+        lateDetail: 'Some late details',
+        timeframes: [1, 2],
+        category: 1
+      },
+      {
+        id: 2,
+        name: 'Another reminder',
+        message: 'Another message',
+        detail: 'Some more details',
+        lateMessage: 'Another late message',
+        lateDetail: 'Some more late details',
+        timeframes: [3, 4, 5],
+        category: 2
+      },
+      {
+        id: 3,
+        name: 'A 3rd reminder',
+        message: 'A 3rd message',
+        detail: 'Yet more details',
+        lateMessage: 'Yet another late message',
+        lateDetail: 'Some more late details',
+        timeframes: [5],
+        category: 3
+      },
+      {
+        id: 4,
+        name: 'A 4th reminder',
+        message: 'A 4th message',
+        detail: 'Yet more details',
+        lateMessage: '',
+        lateDetail: '',
+        timeframes: [5],
+        category: 3
+      }
+    ]));
+
+  });
+
   it('should return an array of all BaseReminders with updated data when updating a baseReminder', function() {
 
     var req = {
@@ -249,6 +313,16 @@ describe('BaseReminders Controller', function() {
         lateDetail: 'Some more late details',
         timeframes: [5],
         category: 4
+      },
+      {
+        id: 4,
+        name: 'A 4th reminder',
+        message: 'A 4th message',
+        detail: 'Yet more details',
+        lateMessage: '',
+        lateDetail: '',
+        timeframes: [5],
+        category: 3
       }
     ]));
 
@@ -290,12 +364,22 @@ describe('BaseReminders Controller', function() {
         lateDetail: 'Some more late details',
         timeframes: [5],
         category: 4
+      },
+      {
+        id: 4,
+        name: 'A 4th reminder',
+        message: 'A 4th message',
+        detail: 'Yet more details',
+        lateMessage: '',
+        lateDetail: '',
+        timeframes: [5],
+        category: 3
       }
     ]));
 
   });
 
-  it('should return a 400 error with a message if not all required fields are provided on a post request', function() {
+  it('should return a 422 error with a message if not all required fields are provided on a post request', function() {
 
     var req = {
       body: {
@@ -324,7 +408,7 @@ describe('BaseReminders Controller', function() {
 
   });
 
-  it('should return a 400 error with a message if not all required fields are provided on a put request', function() {
+  it('should return a 422 error with a message if not all required fields are provided on a put request', function() {
 
     var req = {
       body: {
