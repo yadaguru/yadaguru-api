444c460b46334416b9c1dcdf7991041594386fd9
Aidan Obley <aobley@thoughtworks.com>
2016-05-04 10:15:41 -0400

    [Aidan] Add Phone Number formatting and validation

--- services/usersService.js
+++ services/usersService.js
@@ -1,7 +1,18 @@
 var usersService = function(database) {
 
   var create = function(phoneNumber, callback) {
-    callback(null, { userId: 9 });
+    phoneNumber = phoneNumber.replace(/\D+/g, "");
+    var phoneNumberRegex = /^\d{10}$/;
+
+    if (phoneNumberRegex.test(phoneNumber)) {
+      callback(null, { userId: 9 });
+    } else {
+      var error = {
+        status: 400,
+        message: 'Invalid Phone Number'
+      }
+      callback(error, null);
+    }
   };
 
   return {
--- test/unit/usersService_spec.js
+++ test/unit/usersService_spec.js
@@ -3,26 +3,38 @@
 var assert = require('assert');
 var sinon = require('sinon');
 
-var mockDatabase = {};
-var usersService = require('../../services/usersService.js')(mockDatabase);
-
 describe('Users Service', function() {
+
+  var fakeUserId = 9;
+
+  var mockDatabase = {};
+  mockDatabase.membership = {
+    register: function(argList, callback) {
+      var data = {};
+      data.success = true;
+      data.new_id = fakeUserId;
+      data.message = 'Successfully registered';
+      callback(null, data);
+    }
+  };
+
+  var usersService = require('../../services/usersService.js')(mockDatabase);
+
   it('should return userId if phone_number is valid', function() {
-
-    var fakeUserId = 9;
-
-    mockDatabase.membership = {
-      register: function(argList, callback) {
-        var data = {};
-        data.success = true;
-        data.new_id = fakeUserId;
-        data.message = 'Successfully registered';
-        callback(null, data);
-      }
-    };
-
     var callbackSpy = sinon.spy();
     usersService.create('1234567890', callbackSpy);
+    assert.ok(callbackSpy.calledWith(null, { userId: fakeUserId }));
+  });
+
+  it('should return error if phone_number is invalid', function() {
+    var callbackSpy = sinon.spy();
+    usersService.create('not_a_number', callbackSpy);
+    assert.ok(callbackSpy.calledWith({ status: 400, message: 'Invalid Phone Number' }, null));
+  });
+
+  it('should accept phone numbers with formatting', function() {
+    var callbackSpy = sinon.spy();
+    usersService.create('(123) 456-7890', callbackSpy);
     assert.ok(callbackSpy.calledWith(null, { userId: fakeUserId }));
   });
 });
